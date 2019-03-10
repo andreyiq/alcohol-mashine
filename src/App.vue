ebace7b65b835bede1bbce94aaedb3a4dfb438f1
prgjff <prgjff@gmail.com>
2019-03-10 09:53:27 +0400

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,86 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <div class="t water"><p>{{$store.getters.tWater + '&deg;'}}</p></div>
+    <div class="t tank"><p>{{$store.getters.tTank + '&deg;'}}</p></div>
+    <div class="t defl"><p>{{$store.getters.tDefl + '&deg;'}}</p></div>
+    <div class="t fridge"><p>{{$store.getters.tFridge + '&deg;'}}</p></div>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class App extends Vue {
 }
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
+</script>
+
+<style lang="scss">
+body {
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  font-size: 8pt;
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  display: flex;
+  align-items: center; 
+
+  transform: rotate(90deg); 
+}
+
+#app {
+  display: inline-block;
+  position: relative;
+  border: 1px solid #000000;
+  width: 320px;
+  height: 480px;
+  margin: auto;
+  background: url('./assets/alcohol-mashine.svg');
+  background-size: cover;
+
+  .t {
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    position: absolute;
+    // border: 1px solid red;
+
+    p {
+      font-size: 1.5rem;
+      font-weight: bold;
+      margin: 0;
+      padding: 0;
     }
+  }
+
+  .water {
+    top: 65%;
+    left: 20%;
+    right: 55%;
+    bottom: 10%;
+  }
+
+  .tank {
+    top: 55%;
+    left: 20%;
+    right: 55%;
+    bottom: 35%;
+  }
+
+  .defl {
+    top: 20%;
+    left: 20%;
+    right: 55%;
+    bottom: 60%;
+  }
+
+  .fridge {
+    top: 20%;
+    left: 60%;
+    right: 10%;
+    bottom: 60%;
   }
 }
 </style>
--- src/store.ts
+++ src/store.ts
@@ -3,14 +3,36 @@ import Vuex from 'vuex';
 
 Vue.use(Vuex);
 
+export const INDEX_TEMP_WATER   = 0;
+export const INDEX_TEMP_TANK    = 1;
+export const INDEX_TEMP_DEFL    = 2;
+export const INDEX_TEMP_FRIDGE  = 3;
+
 export default new Vuex.Store({
   state: {
-
+    t: [78.3],
+    heater: 0,
+    pumpDef: 0,
+    pumpFridge: 0,
+  },
+  getters: {
+    tWater: (state) => state.t[INDEX_TEMP_WATER] || 0,
+    tTank: (state) => state.t[INDEX_TEMP_TANK] || 0,
+    tDefl: (state) => state.t[INDEX_TEMP_DEFL] || 0,
+    tFridge: (state) => state.t[INDEX_TEMP_FRIDGE] || 0,
   },
   mutations: {
 
   },
   actions: {
-
+    setTemp(state: any, t: number[]) {
+      state.t = t;
+    },
+    setPumpDef(state: any, value: number) {
+      state.pumpDef = value;
+    },
+    setPumpFridge(state: any, value: number) {
+      state.pumpFridge = value;
+    },
   },
 });
